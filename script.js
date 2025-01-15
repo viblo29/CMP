4d644b6fc7e0380233e080e1055db7d62b81d1ee
petrebagashvili <petre.bagashvili204@ens.tsu.edu.ge>
2025-01-15 22:04:28 +0400

    email created

---
+++ script.js
@@ -0,0 +1,21 @@
+const email = document.getElementById('email');
+const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+document.getElementById('signup-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+
+    let isValid = true;
+
+    if (!emailPattern.test(email.value)) {
+        alert("there is something wrong in your email")
+        isValid = false;
+    } else {
+        isValid = true
+    }
+
+    if (isValid) {
+        alert("Successfully submitted");
+        email.value = " "
+    }
+});
