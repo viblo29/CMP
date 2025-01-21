e8472a2126fd977e645a521f951d29137841c86a
petrebagashvili <petre.bagashvili204@ens.tsu.edu.ge>
2025-01-21 15:43:48 +0400

    CMP_23

--- script.js
+++ script.js
@@ -18,3 +18,21 @@ document.getElementById('signup-form').addEventListener('submit', function (even
     }
 });
 
+const MainBoxes = document.getElementById('mainboxes')
+const button = document.getElementById('browse')
+
+MainBoxes.style.display = "none"
+let visible = false
+
+button.addEventListener('click',function(event){
+    event.preventDefault();
+    setTimeout(() => {
+        if (visible) {
+            MainBoxes.style.display = "flex"
+        } else {
+            MainBoxes.style.display = "none"
+            MainBoxes.scrollIntoView({behavior:'smooth',block:'start'})
+        }
+        visible = !visible
+    }, 500);
+})
